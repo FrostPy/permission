85548dcdfeea6b85e032d2d9cafd7da98d19d6cb
FrostPy <ua_frost@mail.ru>
2022-12-07 20:01:09 +0300

    first commit

---
+++ advertisements/views.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from api_with_restrictions.advertisements.models import Advertisement
+from api_with_restrictions.advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
